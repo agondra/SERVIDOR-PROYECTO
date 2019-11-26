4c352b357e11085d9078593ceb96832bfd27fa6a
alex <alex20122012@hotmail.es>
2019-11-26 22:40:49 +0100

    aut-001 - Subida url mogo db atlas

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     jwtSecret: 'long-live-the-ionic-academy',
-    db: 'mongodb://localhost/ionic-jwt'
+    db: 'mongodb+srv://alex:Ordenador3S@cluster0-bk7gg.mongodb.net/test?retryWrites=true&w=majority'
 };
