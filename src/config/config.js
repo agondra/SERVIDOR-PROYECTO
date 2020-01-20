7ca683e46e939ab9e2281e61bb77cd2588489b18
Alex <gonrualex@gmail.com>
2020-01-20 22:16:40 +0100

    aut-017 Cabmio en el config para que coja mongdb

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     jwtSecret: 'long-live-the-ionic-academy',
- // db: 'mongodb+srv://alex:Ordenador3S@cluster0-bk7gg.mongodb.net/test?retryWrites=true&w=majority',
-   db:'mongodb://localhost/mongodb',
+  db: 'mongodb+srv://alex:Ordenador3S@cluster0-bk7gg.mongodb.net/test?retryWrites=true&w=majority',
+ // db:'mongodb://localhost/mongodb',
     gmail:'Ordenador3S',
     gmailUser:'autorregulacionemocional@gmail.com',
-    urlBase:'http://localhost:5000'
-   // urlBase:'https://servidor-proyecto.herokuapp.com'
+    //urlBase:'http://localhost:5000'
+    urlBase:'https://servidor-proyecto.herokuapp.com'
 };
